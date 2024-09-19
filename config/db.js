2d4e19fa63a7aa4925eec69bf9aaaeffed9dcbce
lokeshbhavsar <lokeshbhavsar127@gmail.com>
2024-09-19 23:46:10 +0530

    server added

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/socialmedia', {
+    await mongoose.connect('mongodb+srv://lokeshsmiles9:NTV6Vys0l4iHrpKL@cluster0.4wv67.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
